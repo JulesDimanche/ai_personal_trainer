7cf4ff79d6c817e4bb553fcb8ba6cf5d24fa5eb3
JulesDimanche <julesdimanche200479@gmail.com>
2025-12-11 11:26:08 +0530

    update the requirements

--- server/run_etl.py
+++ server/run_etl.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from duckdb_etl import etl_incremental
 from weekly_update import run_weekly_adaptation
 etl_incremental()
